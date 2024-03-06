5ae1d85fdbec0136a4a3f69c026f788620de7e49
BobEarl <bobearl@mac.com>
2024-03-05 17:54:06 -0800

    Initial Commit

---
+++ CKShareTester/CKShareTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CKShareTesterApp.swift
+//  CKShareTester
+//
+//  Created by BobEarl on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CKShareTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CKShareTester/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CKShareTester
+//
+//  Created by BobEarl on 3/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
